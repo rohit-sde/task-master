48079f17dddf2e99c2e442e61221b05cd40c2220
Rohit Singh <rohitsinghpb4@gmail.com>
2023-02-09 11:51:15 +0530

    Cross Origin Header are added.

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ if(envConfig.error) console.log(envConfig)
 else env = envConfig.parsed
 
 // Middlewares
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 app.use(express.static("./public"))
 app.use(express.json())
 
